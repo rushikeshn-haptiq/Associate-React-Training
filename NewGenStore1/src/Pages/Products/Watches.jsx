24f1a9c58e3ead5a5f5c40158b86f70a45e75b62
rushikesh-haptiq <rushikesh.nikam@haptiq.com>
2025-08-08 15:12:27 +0530

    check issues

--- NewGenStore1/src/Pages/Products/Watches.jsx
+++ NewGenStore1/src/Pages/Products/Watches.jsx
@@ -1,4 +1,4 @@
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import ProductCard from '../../components/ProductCard';
 import ReviewList from '../../Features/reviewList';
 import { useSelector } from 'react-redux';
